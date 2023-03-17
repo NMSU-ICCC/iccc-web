66226edf94ac2aa4c51b2c7ed9d558a4045d1880
Manuel Mares <manuels@nmsu.edu>
2023-03-17 16:25:29 -0600

    added to WhoAreWe

--- src/Components/AboutUs/WhoAreWe.js
+++ src/Components/AboutUs/WhoAreWe.js
@@ -5,9 +5,62 @@ import {Officer} from "./Officer";
 export class WhoAreWe extends Component{
     render(){
         return(  
-            <div class="px-20 py-40 w-full h-screen">
-                asdfadfasd
+            <div class="m-20 w-full h-screen p-20 items-center justify-center">
+                <div class="bg-white h-fit shadow-lg rounded-lg w-full h-full flex-row flex justify-center items-center">
+                    <div class="w-2/4 h-full bg-[url('Resources/Images/NMSU_3.jpg')] bg-cover rounded-l-lg">
+                    </div>
+                    <div class="w-2/4 p-20">
+                        <h1 class="text-5xl pb-10">Become an Officer</h1>
+                        <p class="text-left">
+                            We are actively looking for people to fill new positions and help us improve the club.
+                            If you are interesting, send an email to any of the officers expressing your interest.
+                            As an officer, you will be able to take part in the creation, planning direction and development
+                            of our projects an activities. You can help other members improve their skill at the same time
+                            that you improve yours as well as adding a cool achivement for your resume!
+                            Moreover, you can have a more active rol deciding what projects to develope.
+                        </p>
+                    </div>
+                </div>
+            
             </div>
+            // <div class="container my-24 px-6 mx-auto">
+            //     <section class="mb-32 text-gray-800">
+            //         <div class="block rounded-lg shadow-lg bg-white">
+            //         <div class="flex flex-wrap items-center">
+            //             <div class="flex h ">
+            //                 <img 
+            //                     class="bg-[url('Resources/Images/NMSU_3.jpg')] bg-no-repeat bg-cover" />
+            //             </div>
+            //             <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 xl:w-8/12">
+            //             <div class="px-6 py-12 md:px-12">
+            //                 <h2 class="text-2xl font-bold mb-4">What's the secret of the great taste?</h2>
+            //                 <p class="uppercase text-red-600 font-bold mb-6 flex items-center">
+            //                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="w-4 h-4 mr-2">
+            //                     <path fill="currentColor"
+            //                     d="M216 23.86c0-23.8-30.65-32.77-44.15-13.04C48 191.85 224 200 224 288c0 35.63-29.11 64.46-64.85 63.99-35.17-.45-63.15-29.77-63.15-64.94v-85.51c0-21.7-26.47-32.23-41.43-16.5C27.8 213.16 0 261.33 0 320c0 105.87 86.13 192 192 192s192-86.13 192-192c0-170.29-168-193-168-296.14z" />
+            //                     </svg>Hot news
+            //                 </p>
+            //                 <p class="text-gray-500 mb-6">
+            //                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum maxime voluptas
+            //                 ipsam aliquam itaque cupiditate provident architecto expedita harum culpa odit,
+            //                 inventore rem molestias laborum repudiandae corporis pariatur quo eius iste!
+            //                 Quaerat, assumenda voluptates! Molestias, recusandae? Maxime fuga omnis ducimus.
+            //                 </p>
+            //                 <p class="text-gray-500">
+            //                 Commodi ut nisi assumenda alias maxime necessitatibus ad rem repellat explicabo,
+            //                 reiciendis illum suscipit iusto? Provident dignissimos similique, reiciendis
+            //                 inventore accusantium unde mollitia, deleniti quae atque error id perspiciatis
+            //                 illum. Laboriosam aperiam ab illo dignissimos obcaecati corporis similique a odio,
+            //                 optio iste quis placeat alias amet rerum sint quos dolor pariatur inventore
+            //                 possimus ad consequuntur fugiat perferendis consectetur laudantium.
+            //                 </p>
+            //             </div>
+            //             </div>
+            //         </div>
+            //         </div>
+            //     </section>
+                
+            //     </div>
             
         )
     }
