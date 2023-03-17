62a890a06b481f88a13b6a5591892161813d226a
Manuel Mares <manuels@nmsu.edu>
2023-03-17 15:50:55 -0600

    new init firebase

--- src/Components/Home/Welcome.js
+++ src/Components/Home/Welcome.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export class Welcome extends Component{
     render() {
       return (   
-        <div class="bg-white  w-screen min-h-screen max-h-full bg-[url('Resources/Images/NMSU_2.jpg')] bg-no-repeat bg-cover p-10 pt-20  items-center justify-center sm:p-10" >
+        <div class="bg-white flex w-screen min-h-screen max-h-full bg-[url('Resources/Images/NMSU_2.jpg')] bg-no-repeat bg-cover p-10 pt-20  items-center justify-center sm:p-10" >
             <div class="bg-white p-10 text-left justify-center items-center flex-col opacity-80 text-2xl text-black w-full h-fit">
                 <p class="text-7xl font-bold" > Welcome </p>
                 <p >
