c78e67a4772ffc1caadb51e2d4bc19bfceba850a
Manuel Mares <manuels@nmsu.edu>
2023-03-21 14:18:13 -0600

    new descriptions

--- src/Components/Home/Activities.js
+++ src/Components/Home/Activities.js
@@ -4,9 +4,9 @@ import {Activity} from "./Activity"
 const activities = [
   ["Mock Interviews", "Weekly interviews are hosted in realistical environments where you will get familiar with solving techniques and will recieve feedback to improve interviewing skills."],
   ["Deploy applications", "All of our projects have the goal of having real life application, and nothing looks better in your resume that having projects people actually use."],  
-  ["Learn Modern Technologies", "React, Git, Firebase, Arduino, Testing... All the things you used in class and never really learn! To help you being a successful developer, we make sure you learn new and relevant technologies."],
-  ["Team projects", "Multiple technologies are combined in different ways to create exciting projects. The multidisciplinary will allow you to get familiar with new areas of development and to learn how to integrate in a team before you graduate."],
-  ["Research", "Our club is not limited to producing software projects. We care about quality an innovation. New ideas are encouraged to be developed and tools are provided so you can develop them successfully."],
+  ["Learn Modern Technologies", "React, Git, Firebase, Arduino, Testing... All the things you used in class and never really learnt!"],
+  ["Team projects", "Multiple technologies are combined in different ways to create exciting projects. Aditionally, gain experience designing and leading a project."],
+  ["Research", "Our club is not limited to producing software projects. We care about quality an innovation. New ideas are encouraged, and tools are provided so you can develop them successfully."],
 ]
 
 export class Activities extends Component{
@@ -25,11 +25,12 @@ export class Activities extends Component{
               <div class="text-center max-w-[700px]">
                   <h2 class="text-3xl text-blue-600  font-bold mb-6">What do we do?</h2>
                   <p class=" mb-12">
-                    ICCC has many different activities to adapt to different members. The activities are design with focus in 
-                    gaining real-world experience and build a strong portfolio of projects. Although the activities are adapted
-                    to the needs of every member individually, we make sure they are compatible with other members' activities. 
-                    In that way, we make sure every member is included, supported, listened to, and that they actively help us 
-                    build ICCC 
+                    
+                    ICCC has many activities with focus in gaining real-world experience and build a strong portfolio of projects. 
+                    Although most activities are set for group work, they are also designed by considering the needs and preferences of each
+                    individual member. 
+                    In that way, we make sure every member is included, supported, benefit from, listened to, and that they actively help us 
+                    build and improve this club 
                   </p>
               </div>
               </div>
