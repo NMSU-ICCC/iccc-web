6957f931ad76345757ca063ecf9b5dc71d05e55e
Manuel Mares <manuels@nmsu.edu>
2023-03-21 09:17:47 -0600

    edited activities

--- src/Components/Home/Activities.js
+++ src/Components/Home/Activities.js
@@ -2,11 +2,11 @@ import React, {Component} from 'react';
 import {Activity} from "./Activity"
 
 const activities = [
-  ["Mock Interviews", "weekly interviews to practice problems an gain confidence"],
-  ["Team projects", "Large projects"],
-  ["Research", "Possibility of publishing"],
-  ["Learn Modern Technologies", "React, Git, etc."],
-  ["Deploy applications", "Get real experience with real applications"],  
+  ["Mock Interviews", "Weekly interviews are hosted in realistical environments where you will get familiar with solving techniques and will recieve feedback to improve interviewing skills."],
+  ["Deploy applications", "All of our projects have the goal of having real life application, and nothing looks better in your resume that having projects people actually use."],  
+  ["Learn Modern Technologies", "React, Git, Firebase, Arduino, Testing... All the things you used in class and never really learn! To help you being a successful developer, we make sure you learn new and relevant technologies."],
+  ["Team projects", "Multiple technologies are combined in different ways to create exciting projects. The multidisciplinary will allow you to get familiar with new areas of development and to learn how to integrate in a team before you graduate."],
+  ["Research", "Our club is not limited to producing software projects. We care about quality an innovation. New ideas are encouraged to be developed and tools are provided so you can develop them successfully."],
 ]
 
 export class Activities extends Component{
@@ -25,8 +25,11 @@ export class Activities extends Component{
               <div class="text-center max-w-[700px]">
                   <h2 class="text-3xl text-blue-600  font-bold mb-6">What do we do?</h2>
                   <p class=" mb-12">
-                  In the International Collaborative Coding Club, we have many differente activities that adapt to each member.
-                  The activities are design with the focus of gain real-world experience and help you build a strong portfolio.
+                    ICCC has many different activities to adapt to different members. The activities are design with focus in 
+                    gaining real-world experience and build a strong portfolio of projects. Although the activities are adapted
+                    to the needs of every member individually, we make sure they are compatible with other members' activities. 
+                    In that way, we make sure every member is included, supported, listened to, and that they actively help us 
+                    build ICCC 
                   </p>
               </div>
               </div>
--- src/Components/Home/Welcome.js
+++ src/Components/Home/Welcome.js
@@ -6,7 +6,7 @@ export class Welcome extends Component{
       return (   
         <div class="bg-white flex w-screen min-h-screen max-h-full bg-[url('Resources/Images/NMSU_2.jpg')] bg-no-repeat bg-cover p-10 pt-20  items-center justify-center sm:p-10" >
             <div class="bg-white p-10 text-left justify-center items-center flex-col opacity-80 text-2xl text-black w-full h-fit">
-                <p class="text-4xl sm:text-7xl sm:font-bold" > Welcome </p>
+                <p class="text-4xl sm:text-7xl sm:font-bold " > Welcome </p>
                 <p >
                   To the International Collaborative Coding Club at NMSU.
                 </p>
