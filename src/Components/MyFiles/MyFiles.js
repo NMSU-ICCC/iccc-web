83f88d0e9f8bbf6e71b8cb7798ed19ca81e533fd
Manuel Mares <manuels@nmsu.edu>
2023-04-01 17:04:43 -0600

    added to login sign in

--- src/Components/MyFiles/MyFiles.js
+++ src/Components/MyFiles/MyFiles.js
@@ -3,47 +3,6 @@ import { Link } from "react-router-dom";
 import firebase from "../../firebase";
 
 export default function LogIn(){
-    const[users, setUsers] = useState([]);
-    const[loading, setLoading] = useState(false);
-
-    const ref =firebase.firestore().collection("users");
-    console.log(ref)
-
-    function getUsers(){
-        setLoading(true);
-        ref.onSnapshot((querySnapshot) => {
-            const items = [];
-            querySnapshot.forEach((doc) => {
-                items.push(doc.data());
-            })
-            setUsers(items);
-            setLoading(false);
-        })
-    }
-
-    useEffect(() => {
-        getUsers();
-    }, [])
-
-
-    if(loading){
-        return <h1>loading...</h1>
-    }
-
-    return(  
-        <div>
-            <h1>users</h1>
-            {
-                users.map((user) =>(
-                    <div>
-                        {user.name}
-                        {user.email}
-                        {user.password}
-                        {user.position}
-                        {user.username}
-                    </div>
-                ))
-            }
-        </div>
-    )
+    //use localstorage to save the session status
+    //https://www.youtube.com/watch?v=7hR7oTpDukc
 }
