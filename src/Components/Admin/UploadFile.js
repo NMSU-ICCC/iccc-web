451b178685e2777b1ea21d3c1fda9b825b4a7b4d
Manuel Mares <manuels@nmsu.edu>
2023-04-13 18:01:43 -0600

    added component for upload image

--- src/Components/Admin/UploadFile.js
+++ src/Components/Admin/UploadFile.js
@@ -1,20 +1,7 @@
 import React, {useState} from 'react';
-import {uploadFile} from "../../firebase";
-import ButtonSolid from '../ButtonSolid';
+import UploadImage from './UploadImage';
 
 export default function UploadFile(){
-    const [file, setfile] = useState(null)
-    const [title, setTitle] = useState("")
-
-    function uploadFileToDataBase(){
-        if(file == null){
-            return
-        }
-        if(title.localeCompare("") == 0){
-            return
-        }
-        uploadFile(title, file, "file")
-    }
     return(
         <div class="w-screen h-screen flex items-center justify-center body-font font-raleway">
             <div class="w-fit bg-white p-10 m-10 flex h-fit flex-col rounded-lg shadow-2xl justify-center items-center  md:flex-row ">
@@ -26,25 +13,7 @@ export default function UploadFile(){
                     </div>
                     <div class="pt-10 flex flex-col justify-center items-center">
 
-                        {/* This label replaces the button styles */}
-                        <label htmlFor="filePicker" class=" w-fit h-fit p-5 rounded-md font-extrabold text-iccc-red whitespace-no-wrap hover:underline hover:cursor-pointer">
-                            Load (.png, .jpg, .svg, .pdf)
-                        </label>
-                        
-                        <input type="text"  class="border-iccc-red border-2 text-iccc-red rounded-lg px-5 my-5" value={title} placeholder="Give it a name"
-                            onChange={e=> {                                   
-                                setTitle(e.target.value)
-                            }}>
-                        </input>
-
-                        {/*This button admits the file, but it is not easy to style it. For that reason, we hide it.
-                            It still works, but looks like the label right about*/}
-                        <input  id="filePicker" class="w-0"
-                                style={{visibility:"hidden"}} type="file" name="filename" accept="image/png, image/jpeg .pdf image/svg" 
-                                onChange={e => {setfile(e.target.files[0])}}
-                        /> 
-
-                        <ButtonSolid clickHandler = {uploadFileToDataBase} title="submit resource"/>
+                        <UploadImage/>
 
                     </div>
                 </div>
---
+++ src/Components/Admin/UploadImage.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import {uploadFile} from "../../firebase";
+import ButtonSolid from '../ButtonSolid';
+
+
+export default function UploadImage(props){
+    
+    const [file, setFile] = useState(null)
+    const [title, setTitle] = useState("")
+
+    function uploadFileToDataBase(){
+        if(file == null){
+            return
+        }
+        if(title.localeCompare("") == 0){
+            return
+        }
+        uploadFile(title, file, "file")
+    } 
+    return(
+        <div class="flex flex-col items-center justify-center border-2 border-iccc-red rounded-lg w-full h-full p-5">
+            <p class="text-3xl font-bold m-1 ">
+                Upload a picture!
+            </p>
+            {/* This label replaces the button styles */}
+            <label htmlFor="filePicker" class=" w-fit h-fit p-5 rounded-md font-extrabold text-iccc-red whitespace-no-wrap hover:underline hover:cursor-pointer">
+            Load (.png, .jpg, .svg, .pdf)
+            </label>
+            
+            <input type="text"  class="border-iccc-red border-2 text-iccc-red rounded-lg px-5 my-5" value={title} placeholder="Give it a name"
+                onChange={e=> {                                   
+                    setTitle(e.target.value)
+                }}>
+            </input>
+
+            {/*This button admits the file, but it is not easy to style it. For that reason, we hide it.
+                It still works, but looks like the label right about*/}
+            <input  id="filePicker" class="w-0"
+                    style={{visibility:"hidden"}} type="file" name="filename" accept="image/png, image/jpeg .pdf image/svg" 
+                    onChange={e => {setFile(e.target.files[0])}}
+                    /> 
+
+            <ButtonSolid clickHandler = {uploadFileToDataBase} title="submit resource"/>
+        </div>
+    )
+}
