6c9fd88e7dc0c7ba9f08c8d9d9f7e52804546df9
ManuelMares <fmms@outlook.com>
2023-09-22 16:24:52 -0600

    Added mock Interviews link, fixed menu and styles

--- src/Components/AboutUs/Officers.js
+++ src/Components/AboutUs/Officers.js
@@ -27,17 +27,17 @@ export default function Officers(){
     ]
     const profiles = [] 
     for (let i = 0; i< officers.length; i++){
-    profiles.push(<Officer name={officers[i][0]} position={officers[i][1]} description={officers[i][2]} image={officers[i][3]} email={officers[i][4]} key={i}/>)
+        profiles.push(<Officer name={officers[i][0]} position={officers[i][1]} description={officers[i][2]} image={officers[i][3]} email={officers[i][4]} key={i}/>)
     }
     
     return(              
         <div className="bg-white py-24 sm:py-32 rounded-lg shadow-lg md:mt-20 ">
-            <div className="mx-auto grid max-w-7xl gap-y-20 gap-x-8 px-6 lg:px-8 xl:grid-cols-3">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our officers</h2>
+            <div className="w-100 grid flex justify-center items-center gap-y-20 gap-x-8 px-6 lg:px-8 xl:grid-cols-3">
                 <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
                     {profiles}
                 </ul>
-                <div className="max-w-2xl">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our officers</h2>
+                <div className="max-w-2xl ">
                     <img class="mt-6 text-lg leading-8" src="./undraw_partying_re_at7f.svg" alt="Our current meetings"/>
                 </div>
             </div>
--- src/Components/WideButton.js
+++ src/Components/WideButton.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export class WideButton extends Component{
     render(){
         return(  
-            <Link to={this.props.route} key={this.props.keyA} className='border-b-white border-b-4  font-bold text-lg text-white divide-y-8 bg-nmsu-crimson w-full h-fit p-8'>
+            <Link to={this.props.route} onClick={()=>{this.props.hideMenu()}} key={this.props.keyA} className='border-b-white border-b-4  font-bold text-lg text-white divide-y-8 bg-nmsu-crimson w-full h-fit p-8'>
                 {this.props.message}
             </Link>
         )
